527a1dedf3b43b22b635f0608c9a841f63ef01e3
Litoso <157988871+LitoLitro@users.noreply.github.com>
2025-04-07 00:36:41 -0500

    Update Ejemplo.java

--- Ejemplo/src/ejemplo/Ejemplo.java
+++ Ejemplo/src/ejemplo/Ejemplo.java
@@ -7,7 +7,7 @@ public class Ejemplo {
     
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");
+        System.out.println("Hasta luego mundo");
         
     }
     
